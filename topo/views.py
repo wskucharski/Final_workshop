5bc3bd5d48f58b6255c64922bdcea6ab5fed5dae
Wojtek <wskucharski@gmail.com>
2019-05-21 18:40:34 +0200

    migrations ratings

--- topo/views.py
+++ topo/views.py
@@ -2,7 +2,7 @@ from django.http import HttpResponse
 from django.shortcuts import render, redirect
 from django.views import View
 from .models import Area, Region, Sector, Crag, Route
-from user.models import RouteRating, CragRating, SectorRating, RegionRating, AreaRating
+from user.models import RouteRating
 from django.db.models import Avg
 
 
@@ -22,49 +22,25 @@ class AreaView(View):
     def get(self, request, area_id):
         area = Area.objects.get(id=area_id)
         area_regions = area.region_set.all
-        rating = AreaRating.objects.aggregate(Avg('rating'))['rating__avg']
-        if rating is not None:
-            avg_rating = round(rating, 1)
-        else:
-            avg_rating = 0
-
-        return render(request, 'area.html', {'area': area, 'area_regions': area_regions, 'avg_rating': avg_rating})
+        return render(request, 'area.html', {'area': area, 'area_regions': area_regions})
 
 class AreaRegionView(View):
     def get(self, request, region_id):
         region = Region.objects.get(id=region_id)
         region_sectors = region.sector_set.all()
-        rating = RegionRating.objects.aggregate(Avg('rating'))['rating__avg']
-        if rating is not None:
-            avg_rating = round(rating, 1)
-        else:
-            avg_rating = 0
-
-        return render(request, 'region.html', {'region': region, 'region_sectors': region_sectors, 'avg_rating': avg_rating})
+        return render(request, 'region.html', {'region': region, 'region_sectors': region_sectors})
 
 class RegionSectorView(View):
     def get(self, request, sector_id):
         sector = Sector.objects.get(id=sector_id)
         sector_crags = sector.crag_set.all()
-        rating = SectorRating.objects.aggregate(Avg('rating'))['rating__avg']
-        if rating is not None:
-            avg_rating = round(rating, 1)
-        else:
-            avg_rating = 0
-
-        return render(request, 'sector.html', {'sector': sector, 'sector_crags': sector_crags, 'avg_rating': avg_rating})
+        return render(request, 'sector.html', {'sector': sector, 'sector_crags': sector_crags})
 
 class SectorCragView(View):
     def get(self, request, crag_id):
         crag = Crag.objects.get(id=crag_id)
         crag_routes = crag.route_set.all()
-        rating = CragRating.objects.aggregate(Avg('rating'))['rating__avg']
-        if rating is not None:
-            avg_rating = round(rating, 1)
-        else:
-            avg_rating = 0
-
-        return render(request, 'crag.html', {'crag': crag, 'crag_routes': crag_routes, 'avg_rating': avg_rating})
+        return render(request, 'crag.html', {'crag': crag, 'crag_routes': crag_routes})
 
 class RouteView(View):
     def get(self, request, route_id):
--- user/models.py
+++ user/models.py
@@ -18,26 +18,6 @@ class RouteRating(models.Model):
     route = models.ForeignKey(Route, on_delete=models.CASCADE)
     rating = models.IntegerField(choices=RATING_CHOICES)
 
-class CragRating(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    crag = models.ForeignKey(Crag, on_delete=models.CASCADE)
-    rating = models.IntegerField(choices=RATING_CHOICES)
-
-class SectorRating(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    sector = models.ForeignKey(Sector, on_delete=models.CASCADE)
-    rating = models.IntegerField(choices=RATING_CHOICES)
-
-class RegionRating(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    region = models.ForeignKey(Region, on_delete=models.CASCADE)
-    rating = models.IntegerField(choices=RATING_CHOICES)
-
-class AreaRating(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    area = models.ForeignKey(Area, on_delete=models.CASCADE)
-    rating = models.IntegerField(choices=RATING_CHOICES)
-
 
 
 
